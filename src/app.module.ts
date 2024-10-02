f9de71e113f7cb3556eff5340a4e8785588e47b5
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-10-02 15:12:08 +0530

    db connection string init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { BookModule } from './book/book.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [BookModule],
+  imports: [
+    BookModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('DB_URI'), // Fetches the database URL from the env file
+      }),
+    })
+  ],
   controllers: [AppController],
   providers: [],
 })
