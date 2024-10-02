1ae0c911900288d863b63634cef3b8599511c19c
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-10-02 15:48:52 +0530

    Post a book, Get all books

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BookService } from './book.service';
+import { Book } from './schemas/book.schemas';
 
 @Controller('book')
 export class BookController {
-    constructor() {}
-    
+    constructor(private bookService: BookService) {}
+
     @Get()
     getHello(): string {
       return "hello book";
     }
+
+    @Get('all')
+    async getAllBooks(): Promise<Book[]> {
+      return this.bookService.findAll();
+    }
+    
+    @Post()
+    async createBook(@Body() book: Book): Promise<Book> {
+        return this.bookService.create(book);
+  }
 }
